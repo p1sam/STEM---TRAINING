a54fcf108373e0be421d6d63893b5205903b6b43
p1sam <p1sam254@gmail.com>
2022-05-19 12:26:30 +0300

    strings in python

---
+++ calc.py
@@ -0,0 +1,17 @@
+f_num = int(input("input the first number "))
+s_num = int(input("enter your second number "))
+multiple = f_num * s_num
+add = f_num + s_num
+sub = f_num - s_num
+div = f_num/s_num
+modulus = f_num%s_num
+
+print("multiple = " + str(multiple) )
+
+print("division = " + str(div))
+
+print("subtraction = " + str(sub))
+
+print("addition = " + str(add))
+
+print('modulus = ' + str(modulus))
---
+++ strings.py
@@ -0,0 +1,23 @@
+name = 'p1_sam'
+msg = """my name is Sam
+         i love playing chess"""
+print(msg)
+city = 'nairobi'
+print(city.upper())
+uni = 'JKUAT'
+print (uni.lower())
+fruit = "apple"
+print(fruit[3:])
+print(fruit[-2:])
+#strip
+f_name = (" sam")
+print(f_name.strip())
+s_name = (" capalot")
+full_name =(f_name + s_name)
+print(full_name)
+print(len(full_name))
+word = "Deoxyribonucleic"
+print(len(word))
+
+x = 100
+print(str(x))
